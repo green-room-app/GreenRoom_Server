3e72428efdef3de2650929b6701380feeb1f97fd
zhtcks <75410527+cotchan@users.noreply.github.com>
2022-08-04 01:54:38 +0900

    project init

---
+++ src/main/java/com/greenroom/greenroomserver/GreenroomServerApplication.java
@@ -0,0 +1,13 @@
+package com.greenroom.greenroomserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenroomServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenroomServerApplication.class, args);
+	}
+
+}
